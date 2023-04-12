4dc11004fcb8355865542dd5b16b2cecbb46806a
Tovintsoa <tovintsoa1337@capri-nc.com>
2023-04-12 18:05:22 +0300

    Creation bdd

--- src/main/java/mg/itu/tovintsoa/tp4banquetovintsoa/ejb/GestionnaireCompte.java
+++ src/main/java/mg/itu/tovintsoa/tp4banquetovintsoa/ejb/GestionnaireCompte.java
@@ -4,6 +4,7 @@
  */
 package mg.itu.tovintsoa.tp4banquetovintsoa.ejb;
 
+import jakarta.annotation.sql.DataSourceDefinition;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -14,6 +15,19 @@ import mg.itu.tovintsoa.tp4banquetovintsoa.entities.CompteBancaire;
  *
  * @author Tovintsoa-Capri
  */
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="Tovintsoa",              // nom et
+    password="tovintsoa", // mot de passe que vous avez donnés lors de la création de la base de données
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true"
+    }
+)
 @Stateless
 public class GestionnaireCompte {
     @PersistenceContext(unitName = "banquePU")
