d4c4b7b20a785ddbd2974106e17ba5f9a2f7d6ac
Tovintsoa <tovintsoa1337@capri-nc.com>
2023-04-12 17:46:44 +0300

    ejb stateless

---
+++ src/main/java/mg/itu/tovintsoa/tp4banquetovintsoa/ejb/GestionnaireCompte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tovintsoa.tp4banquetovintsoa.ejb;
+
+import jakarta.ejb.Stateless;
+
+/**
+ *
+ * @author Tovintsoa-Capri
+ */
+@Stateless
+public class GestionnaireCompte {
+    
+}
